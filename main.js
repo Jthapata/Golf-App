35eb8f364cf42796a9b66ec71ce14fc658d49807
jthapata <jessezap01@gmail.com>
2024-03-01 07:22:14 -0700

    adding bootstrap

---
+++ main.js
@@ -0,0 +1,8 @@
+let mainContent = document.getElementById('main-content')
+
+async function getCourseInfo() {
+    let url = "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json"
+    let response = await fetch(url)
+    let data = await response.json()
+    mainContent.innerHTML = JSON.stringify(data)
+}
