b3d2e5c8ab0467cff2f78c4b5d0bddae44c44d36
jthapata <jessezap01@gmail.com>
2024-03-05 10:05:31 -0700

    added select tee and course to html and js

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ async function getCourseInfo() {
     mainContent.append(JSON.stringify(data))
 }
 getCourseInfo()
+
+// m-2 p-1 rounded-pill
+
+var teeType;
+var courseId;
+
+const tees = document.getElementById('tees')
+const teeBtns = document.querySelectorAll('#tee')
+const courses = document.getElementById('courses')
+const courseBtns = document.querySelectorAll('.course')
+const newPlayerButton = document.getElementById('newPlayer')
+
+teeBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        teeType = button.textContent.toLowerCase()
+        tees.innerHTML =`<h3 class="text-center text-white">Tee Type: ${button.textContent}</h3>`
+        courses.className = 'row'
+        courses.classList.add('justify-content-center')
+    })
+})
+courseBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        courseId = button.id
+        courses.innerHTML = `<h3 class="text-center text-white">Course: ${button.textContent}</h3>`
+        newPlayerButton.className = 'm-2'
+        newPlayerButton.classList.add('p-2', 'rounded-pill')
+    })
+})
