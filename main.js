30889b8bb05823dc118bf8ecc37a6b912ba42427
jthapata <jessezap01@gmail.com>
2024-03-05 10:14:44 -0700

    testing pulling data from api

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-let mainContent = document.getElementById('main-content')
-
-async function getCourseInfo() {
-    let url = "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json"
-    let response = await fetch(url)
-    let data = await response.json()
-    mainContent.append(JSON.stringify(data))
-}
-getCourseInfo()
-
-// m-2 p-1 rounded-pill
-
 var teeType;
 var courseId;
 
@@ -18,6 +6,7 @@ const teeBtns = document.querySelectorAll('#tee')
 const courses = document.getElementById('courses')
 const courseBtns = document.querySelectorAll('.course')
 const newPlayerButton = document.getElementById('newPlayer')
+const mainContent = document.getElementById('main-content')
 
 teeBtns.forEach((button) => {
     button.addEventListener('click', () => {
@@ -33,5 +22,13 @@ courseBtns.forEach((button) => {
         courses.innerHTML = `<h3 class="text-center text-white">Course: ${button.textContent}</h3>`
         newPlayerButton.className = 'm-2'
         newPlayerButton.classList.add('p-2', 'rounded-pill')
+        displayCourseInfo(courseId)
     })
 })
+
+async function displayCourseInfo(id) {
+    let url = `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${id}.json`
+    let response = await fetch(url)
+    let data = await response.json()
+    mainContent.append(JSON.stringify(data.holes[0]))
+}
