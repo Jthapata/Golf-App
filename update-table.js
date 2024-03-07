5c2af4e51c564f8d524863c1e61cb2b3a85ee171
jthapata <jessezap01@gmail.com>
2024-03-06 21:31:37 -0700

    working on update js

--- update-table.js
+++ update-table.js
@@ -6,19 +6,21 @@ let frontOutArray = []
 for (i=0; i<4; i++) {
     frontOutArray.push(document.querySelector(`#player${i+1}FrontOut`))
 }
+
 for (elements of frontElementArray) {
     for (element of elements) {
-        let inputValue = element.querySelector('input')
-        inputValue.addEventListener('change', () => {
-            value = inputValue.value
+        let inputElement = element.querySelector('input')
+        inputElement.addEventListener('change', () => {
+            value = inputElement.value
             for (out of frontOutArray) {
-                if(out.id.includes(element.parentElement.id)) {
-                    // let outValue = Number(out.textContent)
-                    // outValue += value
-                    // out.textContent = outValue
-                    // console.log(out)
+                if(out.id.includes(element.classList[0])) {
+                    let outValue = Number(out.textContent)
+                    outValue += value
+                    out.textContent = outValue
+                    console.log(out)
+                    console.log(element)
                 }
             }
-        }) 
+        })
     }
 }
