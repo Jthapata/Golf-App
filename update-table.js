683ac9a36b4a106030bff41855ad998c30b33e5c
jthapata <jessezap01@gmail.com>
2024-03-07 16:24:33 -0700

    finished user number input funcionality

--- update-table.js
+++ update-table.js
@@ -1,26 +1,63 @@
 let frontElementArray = []
 for (i=0; i<4; i++) {
-    frontElementArray.push(document.querySelectorAll(`.player${i+1}Front`))
+    let elements = document.querySelectorAll(`.player${i+1}Front`)
+    for (item of elements) {
+        frontElementArray.push(item)
+    }
 }
 let frontOutArray = []
 for (i=0; i<4; i++) {
     frontOutArray.push(document.querySelector(`#player${i+1}FrontOut`))
 }
 
-for (elements of frontElementArray) {
-    for (element of elements) {
-        let inputElement = element.querySelector('input')
-        inputElement.addEventListener('change', () => {
-            value = inputElement.value
-            for (out of frontOutArray) {
-                if(out.id.includes(element.classList[0])) {
-                    let outValue = Number(out.textContent)
-                    outValue += value
-                    out.textContent = outValue
-                    console.log(out)
-                    console.log(element)
-                }
+frontElementArray.forEach((input) => {
+    input.addEventListener('change', () => {
+        let value = 0
+        for (element of frontElementArray) {
+            if (element.classList[0] === input.classList[0]) {
+                value += Number(element.value)
             }
-        })
+        }
+        for (element of frontOutArray) {
+            if (element.id.includes(input.classList[0])) {
+                element.textContent = value
+                let inId = `${input.classList[0]}In`
+                let inElement = document.getElementById(inId)
+                inElement.textContent = element.textContent
+            }
+        }
+    })
+})
+
+
+let backElementArray = []
+for (i=0; i<4; i++) {
+    let elements = document.querySelectorAll(`.player${i+1}Back`)
+    for (item of elements) {
+        backElementArray.push(item)
     }
 }
+let backOutArray = []
+for (i=0; i<4; i++) {
+    backOutArray.push(document.querySelector(`#player${i+1}BackOut`))
+}
+backElementArray.forEach((input) => {
+    input.addEventListener('change', () => {
+        let value = 0
+        for (element of backElementArray) {
+            if (element.classList[0] === input.classList[0]) {
+                value += Number(element.value)
+            }
+        }
+        let playerClass = String(input.classList[0])
+        let playerNumber = playerClass.slice(0,-4)
+        let inId = `${playerNumber}FrontIn`
+        let inElement = document.getElementById(inId)
+        value += Number(inElement.textContent)
+        for (item of backOutArray) {
+            if (item.id.includes(input.classList[0])) {
+                item.textContent = value
+            }
+        }
+    })
+})
